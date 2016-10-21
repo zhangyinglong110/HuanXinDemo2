ef9e84c3bf3ad02652a8f0b2df5ece16dc78ea25
zhangyinglong110 <zhangyinglongvip@163.com>
2016-10-21 14:19:23 +0800

    用户的注册与登录

--- app/src/main/java/com/example/huanxindemo/user/LoginFragment.java
+++ app/src/main/java/com/example/huanxindemo/user/LoginFragment.java
@@ -2,13 +2,25 @@ package com.example.huanxindemo.user;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TextInputEditText;
 import android.support.v4.app.DialogFragment;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.example.huanxindemo.R;
+import com.hyphenate.EMCallBack;
+import com.hyphenate.chat.EMClient;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
 
 /**
  * Created by Administrator on 2016/10/20 0020.
@@ -18,6 +30,16 @@ import com.example.huanxindemo.R;
 public class LoginFragment extends DialogFragment {
 
 
+    @BindView(R.id.edit_username)
+    TextInputEditText editUsername;
+    @BindView(R.id.edit_password)
+    TextInputEditText editPassword;
+    @BindView(R.id.progress_bar)
+    ProgressBar progressBar;
+    @BindView(R.id.button_confirm)
+    Button buttonConfirm;
+    private Unbinder unbinder;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -25,8 +47,93 @@ public class LoginFragment extends DialogFragment {
         return inflater.inflate(R.layout.fragment_login, container, false);
     }
 
+
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        unbinder = ButterKnife.bind(this, view);
+    }
+
+    @OnClick(R.id.button_confirm)
+    public void Login() {
+        String username = editUsername.getText().toString().trim();
+        String userpwd = editPassword.getText().toString().trim();
+        handlerLogin(username, userpwd);
+    }
+
+    private void handlerLogin(String username, String userpwd) {
+        //开始loading
+        startLoading();
+        if (TextUtils.isEmpty(username) || TextUtils.isEmpty(userpwd)) {
+            stopLoading();
+            String info = getString(R.string.user_error_not_null);
+            showLoginFail(info);
+            return;
+        }
+        //执行连接
+        EMClient.getInstance().login(username, userpwd, new EMCallBack() {
+            @Override
+            public void onSuccess() {
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        stopLoading();
+                        showLoginSuccess();
+                        dismiss();
+                    }
+                });
+            }
+
+            @Override
+            public void onError(int i, final String s) {
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        stopLoading();
+                        showLoginFail(s);
+                    }
+                });
+            }
+
+            @Override
+            public void onProgress(int i, String s) {
+
+            }
+        });
+
+
+    }
+
+
+    //*******************start视图工作*****************
+    public void startLoading() {
+        setCancelable(false);
+        buttonConfirm.setVisibility(View.INVISIBLE);
+        progressBar.setVisibility(View.VISIBLE);
+    }
+
+    public void stopLoading() {
+        setCancelable(true);
+        buttonConfirm.setVisibility(View.VISIBLE);
+        progressBar.setVisibility(View.INVISIBLE);
+    }
+
+
+    public void showLoginSuccess() {
+        Toast.makeText(getContext(), "登录成功", Toast.LENGTH_SHORT).show();
+    }
+
+    public void showLoginFail(String msg) {
+        String info = getString(R.string.user_error_login_fail, msg);
+        Toast.makeText(getContext(), info, Toast.LENGTH_SHORT).show();
+    }
+
+
+    //*******************endt视图工作*****************
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
     }
 }
--- app/src/main/java/com/example/huanxindemo/user/RegisterFragment.java
+++ app/src/main/java/com/example/huanxindemo/user/RegisterFragment.java
@@ -2,13 +2,25 @@ package com.example.huanxindemo.user;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TextInputEditText;
 import android.support.v4.app.DialogFragment;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.example.huanxindemo.R;
+import com.hyphenate.chat.EMClient;
+import com.hyphenate.exceptions.HyphenateException;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
 
 /**
  * Created by Administrator on 2016/10/20 0020.
@@ -17,6 +29,17 @@ import com.example.huanxindemo.R;
 
 public class RegisterFragment extends DialogFragment {
 
+
+    @BindView(R.id.edit_username)
+    TextInputEditText editUsername;
+    @BindView(R.id.edit_password)
+    TextInputEditText editPassword;
+    @BindView(R.id.progress_bar)
+    ProgressBar progressBar;
+    @BindView(R.id.button_confirm)
+    Button buttonConfirm;
+
+    private Unbinder unbinder;
 
     @Nullable
     @Override
@@ -28,5 +51,84 @@ public class RegisterFragment extends DialogFragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        unbinder = ButterKnife.bind(this, view);
+    }
+
+    @OnClick(R.id.button_confirm)
+    public void register() {
+        String username = editUsername.getText().toString().trim();
+        String userpwd = editPassword.getText().toString().trim();
+        handlerRegister(username, userpwd);
+    }
+
+
+    private void handlerRegister(final String username, final String userpwd) {
+        //开始loading
+        startLoading();
+        if (TextUtils.isEmpty(username) || TextUtils.isEmpty(userpwd)) {
+            stopLoading();
+            String info = getString(R.string.user_error_not_null);
+            showRegisterFail(info);
+            return;
+        }
+        //执行连接
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    EMClient.getInstance().createAccount(username, userpwd);
+                    //注册成功将会执行下面操作
+                    getActivity().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            stopLoading();
+                            showRegisterSuccess();
+                            dismiss();
+                        }
+                    });
+                } catch (final HyphenateException e) {
+                    getActivity().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            stopLoading();
+                            showRegisterFail(e.getDescription());
+                        }
+                    });
+                }
+            }
+        }).start();
+    }
+
+
+    //*******************start视图工作*****************
+    public void startLoading() {
+        setCancelable(false);
+        buttonConfirm.setVisibility(View.INVISIBLE);
+        progressBar.setVisibility(View.VISIBLE);
+    }
+
+    public void stopLoading() {
+        setCancelable(true);
+        buttonConfirm.setVisibility(View.VISIBLE);
+        progressBar.setVisibility(View.INVISIBLE);
+    }
+
+
+    public void showRegisterSuccess() {
+        Toast.makeText(getContext(), "注册成功", Toast.LENGTH_SHORT).show();
+    }
+
+    public void showRegisterFail(String msg) {
+        String info = getString(R.string.user_error_register_fail, msg);
+        Toast.makeText(getContext(), info, Toast.LENGTH_SHORT).show();
+    }
+
+
+    //*******************endt视图工作*****************
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
     }
 }
